7c4322d6c41ccf2eda0cd33e3a309b82e089c914
Artem Matveev <art.matveef2012@yandex.ru>
2018-08-28 00:19:13 +0300

    v0.02 alertdialog on prepodBtn

--- app/src/main/java/com/example/sojertondigma/firstapplication/adapter/ListAdapter.java
+++ app/src/main/java/com/example/sojertondigma/firstapplication/adapter/ListAdapter.java
@@ -2,10 +2,13 @@ package com.example.sojertondigma.firstapplication.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -22,28 +25,29 @@ public class ListAdapter extends BaseAdapter {
     private Context mContext;
     protected LayoutInflater inflater;
     private List<Schedule> mScheduleList;
-/*
-    public class ViewHolder extends ListView.ViewHolder {
 
-        public TextView subjectTextView;
-        public TextView prepodTextView;
-        public TextView roomTextView;
-        public TextView timeFromTextView;
-        public TextView timeTillTextView;
+    /*
+        public class ViewHolder extends ListView.ViewHolder {
 
-        public View layout;
+            public TextView subjectTextView;
+            public TextView prepodTextView;
+            public TextView roomTextView;
+            public TextView timeFromTextView;
+            public TextView timeTillTextView;
 
-        public ViewHolder(View itemView) {
-            super(itemView);
-            layout = itemView;
-            subjectTextView = itemView.findViewById(R.id.subject);
-            //prepodTextView = itemView.findViewById(R.id.prepod);
-            roomTextView = itemView.findViewById(R.id.room);
-            timeFromTextView = itemView.findViewById(R.id.timeFrom);
-            timeTillTextView = itemView.findViewById(R.id.timeTill);
+            public View layout;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                layout = itemView;
+                subjectTextView = itemView.findViewById(R.id.subject);
+                //prepodTextView = itemView.findViewById(R.id.prepod);
+                roomTextView = itemView.findViewById(R.id.room);
+                timeFromTextView = itemView.findViewById(R.id.timeFrom);
+                timeTillTextView = itemView.findViewById(R.id.timeTill);
+            }
         }
-    }
-*/
+    */
     public void add(Collection<Schedule> schedule) {
         mScheduleList.addAll(schedule);
         notifyDataSetChanged();
@@ -57,7 +61,7 @@ public class ListAdapter extends BaseAdapter {
         dbHelper.close();
     }
 
-    public void update(final int position){
+    public void update(final int position) {
         final Schedule schedule = mScheduleList.get(position);
         goToUpdateActivity(schedule.getId());
     }
@@ -65,33 +69,50 @@ public class ListAdapter extends BaseAdapter {
     public ListAdapter(Context context, List<Schedule> myDataset) {
         mContext = context;
         mScheduleList = myDataset;
-        inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View v = convertView;
-        if(v == null){
+        if (v == null) {
             v = inflater.inflate(R.layout.display_schedule_view, parent, false);
         }
         final Schedule schedule = mScheduleList.get(position);
-        String subject = schedule.getmSubject();
-        //String prepod = schedule.getmPrepod();
-        String room = schedule.getmRoom();
-        String timeFrom = schedule.getmTimeFrom();
-        String timeTill = schedule.getmTimeTill();
+        final String[] prepod = schedule.getmPrepod().split(" ");
 
         TextView mSubject = v.findViewById(R.id.subject);
-        //TextView mPrepod = v.findViewById(R.id.prepod);
+        ImageButton mPrepod = v.findViewById(R.id.prepodBtn);
         TextView mRoom = v.findViewById(R.id.room);
         TextView mTimeFrom = v.findViewById(R.id.timeFrom);
         TextView mTimeTill = v.findViewById(R.id.timeTill);
+
         mSubject.setText(schedule.getmSubject());
-        //mPrepod.setText(schedule.getmPrepod());
         mRoom.setText(schedule.getmRoom());
         mTimeFrom.setText(schedule.getmTimeFrom());
         mTimeTill.setText(schedule.getmTimeTill());
+        mPrepod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                View mView = inflater.inflate(R.layout.dialog_prepod, null);
+                final ImageView mPrepodImage = mView.findViewById(R.id.prepodImage);
+                final TextView mFirstName = mView.findViewById(R.id.firstName);
+                final TextView mLastName = mView.findViewById(R.id.lastName);
+                final TextView mOtchestvo = mView.findViewById(R.id.otchestvo);
+
+                try {
+                    mFirstName.setText(prepod[0]);
+                    mLastName.setText(prepod[1]);
+                    mOtchestvo.setText(prepod[2]);
+                } catch (Exception e) {}
+
+                builder.setView(mView);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
         return v;
     }
 /*
@@ -149,17 +170,17 @@ public class ListAdapter extends BaseAdapter {
     }
 
     @Override
-    public Schedule getItem(int position){
+    public Schedule getItem(int position) {
         return mScheduleList.get(position);
     }
 
     @Override
-    public long getItemId(int position){
+    public long getItemId(int position) {
         return position;
     }
 
     @Override
-    public int getCount(){
+    public int getCount() {
         return mScheduleList.size();
     }
 }
